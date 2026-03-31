e1da5c2c906408d8b4686df8509269906dc005ca
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-03-31 12:14:33 +0200

    wtf

--- flake.nix
+++ flake.nix
@@ -2,8 +2,9 @@
   description = "ROS messages which correspond to the loco-3d/linear-feedback-controller package.";
 
   inputs = {
-    gazebros2nix.url = "github:gepetto/gazebros2nix";
-    flakoboros.follows = "gazebros2nix/flakoboros";
+    gazebros2nix.url = "github:nim65s/gazebros2nix/nixos-unstable";
+    gazebros2nix.inputs.flakoboros.follows = "flakoboros";
+    flakoboros.url = "github:gepetto/flakoboros/nixos-unstable";
     flake-parts.follows = "gazebros2nix/flake-parts";
     nixpkgs.follows = "gazebros2nix/nixpkgs";
     systems.follows = "gazebros2nix/systems";
@@ -15,11 +16,13 @@
     inputs.flake-parts.lib.mkFlake { inherit inputs; } (
       { lib, ... }:
       {
+        debug = true;
         systems = import inputs.systems;
         imports = [
           inputs.gazebros2nix.flakeModule
           {
             flakoboros.rosOverrideAttrs.linear-feedback-controller-msgs = _: _: {
+              version = "9.9.9";
               src = lib.fileset.toSource {
                 root = ./.;
                 fileset = lib.fileset.unions [
